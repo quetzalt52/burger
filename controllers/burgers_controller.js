199a52ddec5d91fedaebe2fb8e94cf2e10f14eb7
quetzalt52 <lalys_espinoza@yahoo.com>
2017-08-08 10:23:15 -0700

    worked on media controller and index files

---
+++ controllers/burgers_controller.js
@@ -0,0 +1,40 @@
+var express = require("express");
+var router = express.Router();
+// Import the model (cat.js) to use its database functions.
+var burger = require("../models/burger.js");
+// Create all our routes and set up logic within those routes where required.
+router.get("/", function(req, res) {
+  burger.sellectAll(function(data) {
+    var hbsObject = {
+      burgers: data
+    };
+    console.log(hbsObject);
+    res.render("index", hbsObject);
+  });
+});
+router.post("/index", function(req, res) {
+  burger.insertOne([
+    "burger_name", "devoured"
+  ], [
+    req.body.burger_name, req.body.devoured
+  ], function() {
+    res.redirect("/");
+  });
+});
+router.put("/:id", function(req, res) {
+  var condition = "id = " + req.params.id;
+  console.log("condition", condition);
+  burger.updateOne({
+    devoured: req.body.devoured
+  }, condition, function() {
+    res.redirect("/");
+  });
+});
+router.delete("/:id", function(req, res) {
+  var condition = "id = " + req.params.id;
+  burger.delete(condition, function() {
+    res.redirect("/");
+  });
+});
+// Export routes for server.js to use.
+module.exports = router;
