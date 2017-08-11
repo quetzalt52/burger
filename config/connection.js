a0a831fb9dd6eaccb03d6318c86ec38da07e4a9e
quetzalt52 <lalys_espinoza@yahoo.com>
2017-08-10 22:45:58 -0700

    updated connection file

--- config/connection.js
+++ config/connection.js
@@ -4,7 +4,7 @@ var connection = mysql.createConnection({
   port: 3306,
   host: "localhost",
   user: "root",
-  password: "montana1",
+  password: "",
   database: "burgers_db"
 });
 // Make connection.
